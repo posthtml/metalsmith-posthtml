c1e52ffd2cf018670c5a38976a0097b1a4ab6597
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-08-02 04:00:25 +0200

    refactor(index): cleanup

--- index.js
+++ index.js
@@ -6,21 +6,34 @@
 
 const posthtmlrc = require('posthtml-load-config')
 
-function posthtml (options) {
+function posthtml (plugins, options) {
   return function (files, metalsmith, done) {
-    posthtmlrc()
-      .then(({ plugins, options }) => {
-        Object.keys(files).forEach((file) => {
-          let html = files[file].contents.toString()
+    if (!plugins && !options) {
+      posthtmlrc()
+        .then(({ plugins, options }) => {
+          Object.keys(files).forEach((file) => {
+            let html = files[file].contents.toString()
 
-          require('posthtml')(plugins)
-            .process(html, options)
-            .then((result) => {
-              files[file].contents = new Buffer(result.html)
-              done()
-            })
+            require('posthtml')(plugins)
+              .process(html, options)
+              .then((result) => {
+                files[file].contents = new Buffer(result.html)
+                done()
+              })
+          })
         })
+    } else {
+      Object.keys(files).forEach((file) => {
+        let html = files[file].contents.toString()
+
+        require('posthtml')(plugins)
+          .process(html, options)
+          .then((result) => {
+            files[file].contents = new Buffer(result.html)
+            done()
+          })
       })
+    }
   }
 }
 
