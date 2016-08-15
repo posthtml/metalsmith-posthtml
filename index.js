444ceebf8c5d0082c75b34b63faa6c41722fbe47
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-08-15 04:40:38 +0200

    refactor(index): add jsdoc, code cleanup

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-// ------------------------------------
-// #METALSMITH - POSTHTML
-// ------------------------------------
-
-'use strict'
-
-const posthtmlrc = require('posthtml-load-config')
-
-function posthtml (plugins, options) {
+/**
+ *@module metalsmith-posthtml
+ *@author Michael Ciniawsky (@michael-ciniawsky)
+ *@version 1.0.0
+ *
+ * @requires posthtml
+ * @method posthtml
+ * @param plugins {Array}  PostHTML plugins
+ * @param options {Object} PostHTML options
+ *
+ * @returns {Buffer} html
+ */
+module.exports = function posthtml (plugins, options) {
   return function (files, metalsmith, done) {
-    if (!plugins && !options) {
-      posthtmlrc()
-        .then(({ plugins, options }) => {
-          Object.keys(files).forEach((file) => {
-            let html = files[file].contents.toString()
+    Object.keys(files).forEach((file) => {
+      let html = files[file].contents.toString()
 
-            require('posthtml')(plugins)
-              .process(html, options)
-              .then((result) => {
-                files[file].contents = new Buffer(result.html)
-                done()
-              })
-          })
+      require('posthtml')(plugins)
+        .process(html, options)
+        .then((result) => {
+          files[file].contents = new Buffer(result.html)
+
+          done()
         })
-    } else {
-      Object.keys(files).forEach((file) => {
-        let html = files[file].contents.toString()
-
-        require('posthtml')(plugins)
-          .process(html, options)
-          .then((result) => {
-            files[file].contents = new Buffer(result.html)
-            done()
-          })
-      })
-    }
+    })
   }
 }
-
-module.exports = posthtml
